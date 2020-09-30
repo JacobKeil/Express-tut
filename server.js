1c6d3a3a10fea7c59b4c3b04dfb82f0682f4c172
Jacob Keil <jacobkeil13@gmail.com>
2020-09-30 19:20:36 -0400

    Part 2 commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ejs = require("ejs");
+const computers = require(__dirname + "/views/json/computers.json");
 
 const server = express();
 
@@ -9,6 +10,11 @@ server.set("view engine", "html");
 server.engine("html", ejs.renderFile);
 server.get("/", (req, res) => res.render("index"));
 server.get("/about", (req, res) => res.render("about"));
+server.get("/computers", (req, res) => res.send("computer"));
+
+server.get("/computers/:id", (req, res) =>
+  res.send(computers[req.params.id - 1])
+);
 
 const port = process.env.PORT || 5000;
 
